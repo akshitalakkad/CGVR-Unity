5bb4e06367d267e5ecb78d1f5a43e35954c15719
akshitalakkad <akshita.lakkad@gmail.com>
2018-02-13 11:35:44 +0530

    Add files via upload

---
+++ ScoreScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+
+private int count;
+private Text countText;
+private Text winText;
+
+
+void start()
+{
+		count = 0;
+	winText.text = " ";
+
+	SetCountText();
+}
+
+void SetCountText()
+{
+	countText.text = "Coins: " + count.ToString();
+	if(count==3)
+	{
+		winText.text = "You Win!";
+	}
+}
+		
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Collectible"))
+			{
+			count = count + 1;
+				other.gameObject.SetActive (false);
+			SetCountText ();
+		}
+	}
+
+}
